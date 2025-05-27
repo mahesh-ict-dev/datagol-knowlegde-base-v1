15f95b0bcb88ca5e4df96f718447ff3cf44bb176
Maheshin <mahesh-ict-dev@users.noreply.github.com>
2025-05-28 01:08:25 +0530

    adding the link and background

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -140,7 +140,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} DataGOL`,
       },
       prism: {
         theme: prismThemes.github,
