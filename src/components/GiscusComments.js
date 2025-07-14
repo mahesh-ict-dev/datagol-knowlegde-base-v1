f8a270d6201a96b36566bcefd81b03fd3f14b67d
Maheshin <mahesh-ict-dev@users.noreply.github.com>
2025-07-14 19:00:33 +0530

    new changes

---
+++ src/components/GiscusComments.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+export default function GiscusComments() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://giscus.app/client.js';
+    script.setAttribute('data-repo', 'mahesh-ict-dev/datagol-knowlegde-base-v1');
+    script.setAttribute('data-repo-id', 'R_kgDOOthykw');
+    script.setAttribute('data-category', 'General');
+    script.setAttribute('data-category-id', 'DIC_kwDOOthyk84Cs7ia');
+    script.setAttribute('data-mapping', 'pathname');
+    script.setAttribute('data-strict', '1');
+    script.setAttribute('data-reactions-enabled', '1');
+    script.setAttribute('data-emit-metadata', '0');
+    script.setAttribute('data-theme', 'light');
+    script.setAttribute('crossorigin', 'anonymous');
+    script.async = true;
+
+    const giscusContainer = document.getElementById('giscus-container');
+    if (giscusContainer) {
+      giscusContainer.innerHTML = '';
+      giscusContainer.appendChild(script);
+    }
+  }, []);
+
+  return <div id="giscus-container" style={{ marginTop: '2rem' }} />;
+}
