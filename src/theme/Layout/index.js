16de691add85769927f9ad920fc401c3b6e18168
Ubuntu <ubuntu@ip-10-1-0-70.ec2.internal>
2025-07-23 17:00:47 +0000

    added new chatjs

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -7,10 +7,11 @@ export default function LayoutWrapper(props) {
     if (!existingScript) {
       const script = document.createElement('script');
       script.id = 'letzask-chatbot';
-      script.src = 'https://d3ua5pgefbvm1i.cloudfront.net/chatbot_widget/chatbot-widget.js';
+      script.src = 'https://d3ua5pgefbvm1i.cloudfront.net/chatbot.js';
       script.async = true;
-      script.setAttribute('data-agent-id', 'YOUR_AGENT_ID'); // <-- replace with actual ID
-      script.setAttribute('data-app-id', 'YOUR_APP_ID');     // <-- replace with actual ID
+      script.setAttribute('data-host', 'https://workbook.datagol.ai');
+      script.setAttribute('data-agent-id', '3ab4f616-814a-4bc7-9b2f-9d0d89b9a83b'); // <-- replace with actual ID
+      script.setAttribute('data-app-id', 'fc59016f-49b6-43c2-8fc2-c9ca958c6dab');     // <-- replace with actual ID
       document.body.appendChild(script);
     }
   }, []);
