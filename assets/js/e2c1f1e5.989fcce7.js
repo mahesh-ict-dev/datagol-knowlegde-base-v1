27b6f0f70b74c5878ded0b1697482ea6f45fffe8
lizy-ict-docs <lizy-ict-docs@users.noreply.github.com>
2025-08-20 12:54:20 +0000

    deploy: 90d9f0733b5502979f41dd7f6ff1242974913c9a

---
+++ assets/js/e2c1f1e5.989fcce7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdg_knowlege_base=self.webpackChunkdg_knowlege_base||[]).push([[3235],{64971:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/datagol-knowlegde-base-v1/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
